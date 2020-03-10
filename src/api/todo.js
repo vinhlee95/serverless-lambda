3ce32b8b08c7b80694e4cbcae550001930d9ffed
Vinh Le <vinh@vertics.co>
2020-03-10 06:22:01 +0200

    feat: add handlers for GET todo by id params

---
+++ src/api/todo.js
@@ -0,0 +1,18 @@
+module.exports.handler = async (event) => {
+  const todoId = event.pathParameters.id
+  const todos = [
+		{id: 1, name: 'clean up', status: 'open'},
+		{id: 2, name: 'cook', status: 'done'}
+	]
+  const todo = todos.find(todo => todo.id == todoId)
+
+  if(!todo) return {
+    statusCode: 404,
+    body: JSON.stringify({message: 'Cannot find any todo'})
+  }
+
+	return {
+		statusCode: 200,
+		body: JSON.stringify(todo)
+	}
+}
