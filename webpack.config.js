59204b98afa064d2d6551f0a3fd47b8cb72480fc
Vinh Le <vinh@vertics.co>
2020-03-11 07:04:51 +0200

    chore: remove webpack node external and change includeModules to true

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,17 @@
 const path = require('path');
 const slsw = require('serverless-webpack');
-const nodeExternals = require('webpack-node-externals');
 
 module.exports = {
 	entry: slsw.lib.entries,
 	target: 'node',
 	mode: slsw.lib.webpack.isLocal ? 'development': 'production',
 	optimization: {
-		// We no not want to minimize our code.
-		minimize: false
+		minimize: true
 	},
 	performance: {
-		// Turn off size warnings for entry points
 		hints: false
 	},
 	devtool: 'nosources-source-map',
-	externals: [nodeExternals()],
 	module: {
 		rules: [
 			{
