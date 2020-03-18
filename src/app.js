e6d68c6fe87aa3cd6cf4b726e55844ee8287c86c
Vinh Le <vinh@vertics.co>
2020-03-18 22:49:01 +0200

    refactor: remove todo module

--- src/app.js
+++ src/app.js
@@ -1,15 +1,20 @@
 import express from 'express'
+import bodyParser from 'body-parser'
+
 import * as todos from './controllers/todos'
-import * as todo from './controllers/todo'
 
 const app = express()
+app.use(bodyParser.json({strict: false}))
 
 app.get('/', (req, res) => res.status(200).send('OK'))
 app.get('/todos', (req, res) => {
 	todos.getAll(req, res)
 })
 app.get('/todos/:id', (req, res) => {
-	todo.getById(req, res)
+	todos.getById(req, res)
+})
+app.post('/todos', (req, res) => {
+	todos.createOne(req, res)
 })
 
 export default app
